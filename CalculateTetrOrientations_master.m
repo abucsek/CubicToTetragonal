50b91c265e58a6d567a0b38d0030594d88ea2657
Ashley Bucsek <ashley.n.bucsek@gmail.com>
2018-06-20 11:33:55 -0500

    Add files via upload

--- CalculateTetrOrientations_master.m
+++ CalculateTetrOrientations_master.m
@@ -5,6 +5,11 @@
 %
 % Outputs: tetragonal orientations for each correspondence variant (CV) in
 % the twin pair of interest
+% For each twin pair i-j, there are two types of twins, and for each type
+% of twin, there are two types of habit planes (cubic/tetragonal phase
+% interfaces). So there are 4 solutions per twin pair. This code outputs
+% the orientation for both i and j for each of the 4 solutions, totaling 8
+% tetragonal orientations. 
 %
 % Requires the files tetrOrientations.m, cubic_to_tetr.m, polardecomp.m,
 % and unit.m to be in the file path
@@ -46,9 +51,31 @@ for ii = TwinPairOfInterest
     [nI, nII, RtwinI, RtwinII, mplusI, mminusI, mplusII, mminusII, RhabitplusI, RhabitminusI, RhabitplusII, RhabitminusII] ...
         = cubic_to_tetr(aC, aT, cT, CVPairs(ii,1), CVPairs(ii,2));
     
-    RTetr = tetrOrientations(aC, aT, cT, U(:,:,CVPairs(ii,1)), U(:,:,CVPairs(ii,2)), RtwinI, RhabitplusI);
+    % Twin type I, Habit solution (+)
+    RTetrTmp = tetrOrientations(aC, aT, cT, U(:,:,CVPairs(ii,1)), U(:,:,CVPairs(ii,2)), RtwinI, RhabitplusI);
     for jj = 1 : 2
         % Rotate by cubic orientation
-        RTetr(:,:,jj) = R_C * RTetr(:,:,jj);
+        RTetr(:,:,jj) = R_C * RTetrTmp(:,:,jj);
     end
+
+    % Twin type I, Habit solution (-)
+    RTetrTmp = tetrOrientations(aC, aT, cT, U(:,:,CVPairs(ii,1)), U(:,:,CVPairs(ii,2)), RtwinI, RhabitminusI);
+    for jj = 1 : 2
+        % Rotate by cubic orientation
+        RTetr(:,:,jj+2) = R_C * RTetrTmp(:,:,jj);
+    end    
+    
+    % Twin type II, Habit solution (+)
+    RTetrTmp = tetrOrientations(aC, aT, cT, U(:,:,CVPairs(ii,1)), U(:,:,CVPairs(ii,2)), RtwinII, RhabitplusII);
+    for jj = 1 : 2
+        % Rotate by cubic orientation
+        RTetr(:,:,jj+4) = R_C * RTetrTmp(:,:,jj);
+    end    
+    
+    % Twin type II, Habit solution (-)
+    RTetrTmp = tetrOrientations(aC, aT, cT, U(:,:,CVPairs(ii,1)), U(:,:,CVPairs(ii,2)), RtwinII, RhabitminusII);
+    for jj = 1 : 2
+        % Rotate by cubic orientation
+        RTetr(:,:,jj+6) = R_C * RTetrTmp(:,:,jj);
+    end        
 end
